76b6006d2561927fef46ac7cd4e67fe539d5864b
mamun <abdullah.ssc13@gmail.com>
2021-01-04 12:35:28 +0600

    insert data by queue job batching

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\SalesCsvProcess;
-use App\Models\Sale;
 use Illuminate\Http\Request;
 
 class SalesController extends Controller
@@ -22,30 +21,20 @@ class SalesController extends Controller
             // chunking file
             $chunks = array_chunk($data, 1000);
 
-            // upload csv file
-            foreach ($chunks as $key => $chunk) {
-                $name = "/temp{$key}.csv";
-                $path = resource_path('temp');
-                file_put_contents($path . $name, $chunk);
-            }
-
-            // store data
-            $path = resource_path('temp');
-            $files = glob("$path/*.csv");
-
             $header = [];
 
-            foreach ($files as $key => $file) {
-                $data = array_map('str_getcsv', file($file));
+            foreach ($chunks as $key => $chunk) {
 
-                if ($key === 0) {
-                    $header = $data[0];
-                    unset($data[0]);
-                }
+                /*if ($key < 2 ) {*/
+                    $data = array_map('str_getcsv', $chunk);
 
-                SalesCsvProcess::dispatch($data, $header);
+                    if ($key === 0) {
+                        $header = $data[0];
+                        unset($data[0]);
+                    }
 
-                unlink($file);
+                    SalesCsvProcess::dispatch($data, $header);
+               /* }*/
             }
 
             return 'stored';
--- app/Jobs/SalesCsvProcess.php
+++ app/Jobs/SalesCsvProcess.php
@@ -3,12 +3,13 @@
 namespace App\Jobs;
 
 use App\Models\Sale;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Throwable;
 
 class SalesCsvProcess implements ShouldQueue
 {
@@ -35,9 +36,32 @@ class SalesCsvProcess implements ShouldQueue
      */
     public function handle()
     {
+        $sale_data_array = [];
+
         foreach ($this->data as $sale) {
             $sale_data = array_combine($this->header, $sale);
-            Sale::create($sale_data);
+
+            $sale_data = array_merge($sale_data, [
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+
+            $sale_data_array[] = $sale_data;
         }
+
+        Sale::insert($sale_data_array);
+    }
+
+
+    /**
+     * Handle a job failure.
+     *
+     * @param Throwable $exception
+     * @return void
+     */
+    public function failed(Throwable $exception)
+    {
+        info($exception->getMessage());
+        // Send user notification of failure, etc...
     }
 }
